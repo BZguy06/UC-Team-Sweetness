8162251883288af1ad698efc9bdf900c375e924e
Sean Price <ptorian@gmail.com>
2009-10-06 16:59:06 -0400

    Event and PriorityQueue updated

--- Project 1/Event.java
+++ Project 1/Event.java
@@ -3,15 +3,19 @@ package priorityqueue;
 /**
  * class Event
  * 
- * void setPriority(int newPriority)
+ * Event()
+ *     Default constructor
+ * Event(int newPriority, String newInfo)
+ *     Initializes variables 
+ * public void setPriority(int newPriority)
  *     newPriority - integer from 0 to 200, anything outside bounds throws error
  *     Set the priority of the event
- * int getPriority()
+ * public int getPriority()
  *     Returns the priority of the event
- * void setInfo(String newInfo)
+ * public void setInfo(String newInfo)
  *     newInfo - New info string, over length of 5 throws error
  *     Set the event info
- * String getInfo()
+ * public String getInfo()
  *     Returns the info string for the event
  *     
  * @author Sean
@@ -19,40 +23,41 @@ package priorityqueue;
  */
 
 public class Event {
-	private int p_priority;
-	private String p_info;
+	private int priority;
+	private String info;
 	
-	Event() {
-		p_priority = 0;
-		p_info = "";
+	public Event() {
+		priority = 0;
+		info = "";
 	}
 	
-	void setPriority(int newPriority) throws RuntimeException {
+	public Event(int newPriority, String newInfo) {
+		setPriority(newPriority);
+		setInfo(newInfo);
+	}
+	
+	public void setPriority(int newPriority) throws RuntimeException {
 		if(newPriority < 0 || newPriority > 200) {
 			throw new RuntimeException();
 		}
 		
-		p_priority = newPriority;
+		priority = newPriority;
 	}
 	
-	int getPriority() {
-		return p_priority;
+	public int getPriority() {
+		return priority;
 	}
 	
-	void setInfo(String newInfo) throws RuntimeException {
+	public void setInfo(String newInfo) throws RuntimeException {
 		if (newInfo.length() > 5) {
 			throw new RuntimeException();
 		}
 		
-		p_info = newInfo;
+		info = newInfo;
 	}
 	
-	String getInfo() {
-		return p_info;
-	}
-	
-	String toString() {
-		return p_info+" ";
+	public String getInfo() {
+		return info;
 	}
 	
 }
--- Project 1/PriorityQueue.java
+++ Project 1/PriorityQueue.java
@@ -1,5 +1,128 @@
 package priorityqueue;
 
-public class PriorityQueue {
+/**
+ * class PriorityQueue
+ * 
+ * requires java.util.ArrayList
+ * 
+ * PriorityQueue()
+ *     Default constructor
+ * public void push(Event newEvent)
+ *     newEvent - new event to add
+ *     Adds the new event to the queue and restructures the priority queue
+ * public Event pop()
+ *     Returns the top event in the priority queue and removes it from the queue
+ * private void reorderDown(int pos) {
+ *     pos - position of event to reorder in the priority queue
+ *     Reorders the event tree based on priority from the top to the bottom, starting at [pos]
+ * private void reorderUp(int pos) {
+ *     pos - position of event to reorder in the priority queue
+ *     Reorders the event tree based on priority from the bottom to the top, starting at [pos]
+ * private void swap(int pos_1, int pos_2) {
+ *     pos_1 - position of one event in the queue to swap
+ *     pos_2 - position of the other event in the queue to swap
+ *     Swaps the events in the queue with positions given by [pos_1] and [pos_2]
+ *     
+ * @author Sean
+ *
+ */
 
+import java.util.ArrayList;
+
+public class PriorityQueue {
+	private ArrayList<Event> queue;
+	
+	public PriorityQueue() {
+		queue = new ArrayList<Event>();
+	}
+	
+	public void push(Event newEvent) {
+		queue.add(newEvent);
+		int newIndex = queue.size() - 1;	// This is the index of the element just added
+		reorderUp(newIndex);					// Test new element to make sure it's in the right spot in the queue
+	}
+	
+	public Event pop() throws IndexOutOfBoundsException {
+		if (queue.size() == 0) {
+			throw new IndexOutOfBoundsException();
+		} else {
+			Event eventToReturn = queue.get(0);
+			
+			int lastElementIndex = queue.size() - 1;
+			queue.set(0, queue.get(lastElementIndex));	// Replace first element with last element
+			queue.remove(lastElementIndex);
+			reorderDown(0);
+					
+			return eventToReturn;
+		}
+	}
+	
+	private void reorderDown(int pos) {
+		int queueLength = queue.size();
+		
+		int leftChild = 2 * pos + 1;
+		int rightChild = 2 * pos + 2;
+		
+		if (leftChild >= queueLength && rightChild >= queueLength) { // element has no children
+			return;
+		} else if (leftChild < queueLength && rightChild >= queueLength) {	// element has left child
+			int thisPriority = queue.get(pos).getPriority();
+			int leftChildPriority = queue.get(leftChild).getPriority();
+			
+			if (thisPriority > leftChildPriority) {
+				swap(pos, leftChild);
+				reorderDown(leftChild);
+			}
+		} else {
+			int thisPriority = queue.get(pos).getPriority();
+			int leftChildPriority = queue.get(leftChild).getPriority();
+			int rightChildPriority = queue.get(rightChild).getPriority();
+			
+			if (rightChildPriority < leftChildPriority) {
+				if (thisPriority > rightChildPriority) {
+					swap(pos, rightChild);
+					reorderDown(rightChild);
+				}
+			} else {
+				if (thisPriority > leftChildPriority) {
+					swap(pos, leftChild);
+					reorderDown(leftChild);
+				}
+			}
+		}
+	}
+	
+	private void reorderUp(int pos) {
+		if (pos == 0) {
+			return;						// It's in the right spot
+		} else {
+			int parent = (pos - 1) / 2;
+			if (queue.get(pos).getPriority() < queue.get(parent).getPriority()) {
+				swap(pos, parent);
+				reorderUp(parent);
+			}
+		}
+	}
+	
+	private void swap(int pos_1, int pos_2) throws RuntimeException {
+		if (pos_1 >= queue.size() || pos_2 >= queue.size()) {
+			throw new RuntimeException();
+		}
+		
+		Event temp;
+		temp = queue.get(pos_1);
+		queue.set(pos_1, queue.get(pos_2));
+		queue.set(pos_2, temp);
+	}
+	
+	/* //Used for debugging purposes only
+	public String dumpArray() {
+		String returnStr = "";
+		int queueSize = queue.size();
+		for (int x = 0; x < queueSize; ++x) {
+			returnStr += queue.get(x).getPriority() + ",";
+		}
+		return returnStr;
+	}
+	*/
 }
