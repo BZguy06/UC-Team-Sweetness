2eab2dcb11bba02b8509322fa346509ac50b724e
Sean Price <ptorian@gmail.com>
2009-10-06 15:25:54 -0400

    Event class

--- Project 1/Event.java
+++ Project 1/Event.java
@@ -1,5 +1,36 @@
 package priorityqueue;
 
 public class Event {
-
+	private int p_priority;
+	private String p_info;
+	
+	Event() {
+		p_priority = 0;
+		p_info = "";
+	}
+	
+	void setPriority(int newPriority) throws RuntimeException {
+		if(newPriority < 0 || newPriority > 200) {
+			throw new RuntimeException();
+		}
+		
+		p_priority = newPriority;
+	}
+	
+	int getPriority() {
+		return p_priority;
+	}
+	
+	void setInfo(String newInfo) throws RuntimeException {
+		if (newInfo.length() > 5) {
+			throw new RuntimeException();
+		}
+		
+		p_info = newInfo;
+	}
+	
+	String getInfo() {
+		return p_info;
+	}
+	
 }
